5222c006b011c4468cc31261014c23c595af25cd
GHCM140407 <00001089749521sp@al.educacao.sp.gov.br>
2024-05-21 11:40:43 -0300

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function facaCompras(livroA, livroB) {
+
+  let soma = livroA + livroB;
+  console.log("A soma dos preços dos livros é: R$ " + soma + ",00");
+}
+
+facaCompras(50, 50);
+facaCompras(72, 28); 
+
